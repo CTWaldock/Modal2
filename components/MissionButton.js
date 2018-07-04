5578873ccac69131efeba5f7f20018d16bb7e96d
Clayton-ty <c.t.waldock@outlook.com>
2018-07-04 10:46:58 +1000

    initial commit with modal built and missionButton in production

---
+++ components/MissionButton.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import { Text, TouchableOpacity, View } from 'react-native';
+
+const MissionButton = ({ onPress, missionDate, missionTime, missionName, missionLocation }) => {
+  const { buttonStyle, contentLeft, contentRight, buttonText } = styles;
+
+  return (
+    <TouchableOpacity onPress={onPress} style={buttonStyle}>
+      <View style={contentLeft}>
+        <Text style={buttonText}>
+          {missionDate}
+        </Text>
+      
+        <Text style={buttonText}>
+          {missionTime}
+        </Text>
+      </View>
+
+      <View style={contentRight}>
+        <Text style={buttonText}>
+          {missionName}
+        </Text>
+
+        <Text style={buttonText}>
+          {missionLocation}
+        </Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+const styles = {
+  buttonText: {
+    color: '#FFFFFF',
+    // fontFamily: 'SourceSansPro',
+    fontSize: 28,
+    // fontWeight: 400,
+    // letterSpacing: -1.5,
+    lineHeight: 60,
+    textAlign: 'left',
+    paddingTop: 10,
+    paddingBottom: 10
+  },
+
+  contentLeft: {
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    flex: 1,
+    borderColor: '#888',
+    borderRightWidth: 1,
+  },
+
+  contentRight: {
+    alignItems: 'flex-start',
+    flex: 2,
+    justifyContent: 'space-around',
+    paddingLeft: 10,
+  },
+
+  buttonStyle: {
+    flexDirection: 'row',
+    height: 104,
+    alignSelf: 'stretch',
+    backgroundColor: '#2DCD4F',
+    borderRadius: 6,
+    shadowColor: '#888',
+    shadowOffset: {width: 0, height: 2},
+    shadowOpacity: 0.5,
+    marginLeft: 5,
+    marginRight: 5
+  }
+};
+
+export default MissionButton;
